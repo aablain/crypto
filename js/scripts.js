dfefe2b56d6852e0f12d29fc108bc6dfd1d36ffc
Austin Blain and Ellie Angell <ellie@>
2016-10-27 16:06:01 -0700

    mix up letters and combine them intonew string

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,35 @@
 function jumbled(cryptoInput) {
   debugger;
 
-  var nonAlpha = cryptoInput.replace(/[^a-z]+/gi, '');
-  var lower = nonAlpha.toLowerCase();
+  var cryptoString = "";
+  var coordinates = new Array();
+  var nonAlpha = cryptoInput.toLowerCase().replace(/[^a-z]+/gi, '');
+  var sentance = nonAlpha.split("");
+  var counter = 0;
+  var counter2 = 0;
+  var columns = sentance.length;
+  columns = Math.floor(Math.sqrt(columns));
+  var row = columns + 1;
 
-  return lower;
+  for (ro = 0; ro < row; ro++) {
+    coordinates[ro]=new Array();
+    for (col = 0; col < columns; col++) {
+      coordinates[ro][col] = sentance[counter];
+      counter++;
+    }
+  };
+  for (col = 0; col < columns; col++) {
+    for (ro = 0; ro < row; ro++) {
+      if (coordinates[ro][col] !==undefined) {
+        cryptoString+= coordinates[ro][col];
+        counter2++;
+      // }
+      // if (counter2 % 6 === 0) {
+        cryptoString+=" ";
+       }
+    }
+  };
+  return cryptoString;
 
 };
 
